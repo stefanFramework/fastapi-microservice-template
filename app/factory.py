537096b4687dd524d32b7d29681765e11991d730
stefanFramework <cesar_cappetto@yahoo.com.ar>
2025-03-08 22:07:29 -0300

    CORS

--- app/factory.py
+++ app/factory.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.middlewares.auth import AuthMiddleware
 from app.api.routes.users import router as users_router
 
@@ -10,6 +11,16 @@ def create_app():
 
 
 def create_api(app):
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=[
+            "http://localhost",
+            "http://localhost:8000",
+        ],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
     app.add_middleware(AuthMiddleware)
 
     app.include_router(users_router)
